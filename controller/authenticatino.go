906234da2254fc9b3cd0dd642da3923509a2f283
amirhosein <amirhoseinh1373@gmail.com>
2023-01-05 17:28:11 +0330

    [FEAT]: add register, validate password and get method

---
+++ controller/authenticatino.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"diary_api/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(context *gin.Context) {
+	var input model.Authentication
+
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := model.User{
+		Username: input.Username,
+		Password: input.Password,
+	}
+
+	savedUser, err := user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"user": savedUser})
+}
